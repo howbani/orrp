2fec15163c792e64da7bd05224b8d1cd0dc8e354
Ammar Hawbani <33313489+howbani@users.noreply.github.com>
2021-01-20 11:22:41 +0800

    Add files via upload

---
+++ ui/UIExpermentResults.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ORR.Charts;
+using ORR.Intilization;
+using ORR.ExpermentsResults;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace ORR.ui
+{
+    /// <summary>
+    /// Interaction logic for ExpermentResults.xaml
+    /// </summary>
+    public partial class UIExpermentResults : Window 
+    {
+        public UIExpermentResults() 
+        {
+            InitializeComponent();
+            /*
+            List<CompareResult> list = new List<CompareResult>();
+            CompareResult res = CompareResultsClass.GetEnergyDelayForAnExperment();
+            list.Add(res);
+            dg_data.ItemsSource = list;*/
+        }
+    }
+}
